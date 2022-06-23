47cde0d5e3b2fd2a0d89a0d815c1d3ea6ee4d531
jay <jay.wekeep@gmail.com>
2022-06-23 09:22:18 +0900

    lombok

--- src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
@@ -1,6 +1,7 @@
 package com.fastcampus.springbootpractice;
 
 import com.fastcampus.springbootpractice.properties.MyProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,26 +11,16 @@ import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
+@RequiredArgsConstructor
 @ConfigurationPropertiesScan
 // @Configuration annotation 생략할 경우
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
-    private final Integer height;
-    private final Environment env;
-    private final ApplicationContext applicationContext;
+//    private final Integer height;
+//    private final Environment env;
+//    private final ApplicationContext applicationContext;
     private final MyProperties myProperties;
-
-    public SpringBootPracticeApplication(
-            @Value("${my.height}") Integer height,
-            Environment env,
-            ApplicationContext applicationContext,
-            MyProperties myProperties) {
-        this.height = height;
-        this.env = env;
-        this.applicationContext = applicationContext;
-        this.myProperties = myProperties;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPracticeApplication.class, args);
@@ -37,9 +28,9 @@ public class SpringBootPracticeApplication {
 
     @PostConstruct
     public void asd() {
-        System.out.println("[@Value] : " + height);
-        System.out.println("[Environment] : " + env.getProperty("my.height"));
-        System.out.println("[ApplicationContext] : " + applicationContext.getEnvironment().getProperty("my.height"));
+//        System.out.println("[@Value] : " + height);
+//        System.out.println("[Environment] : " + env.getProperty("my.height"));
+//        System.out.println("[ApplicationContext] : " + applicationContext.getEnvironment().getProperty("my.height"));
         System.out.println("[ConfigurationProps] : " + myProperties.getHeight());
     }
 }
