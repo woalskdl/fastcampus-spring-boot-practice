5b80105196ee28a9dfbc81cda6b2135a72cff7c0
jay <jay.wekeep@gmail.com>
2022-06-27 08:50:21 +0900

    Spring Cache (1)

--- src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/SpringBootPracticeApplication.java
@@ -1,16 +1,21 @@
 package com.fastcampus.springbootpractice;
 
 import com.fastcampus.springbootpractice.properties.MyProperties;
+import com.fastcampus.springbootpractice.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
+@EnableCaching
 @RequiredArgsConstructor
 @ConfigurationPropertiesScan
 // @Configuration annotation 생략할 경우
@@ -21,16 +26,25 @@ public class SpringBootPracticeApplication {
 //    private final Environment env;
 //    private final ApplicationContext applicationContext;
     private final MyProperties myProperties;
+    private final StudentService studentService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootPracticeApplication.class, args);
     }
 
-    @PostConstruct
+    /*@PostConstruct
     public void asd() {
 //        System.out.println("[@Value] : " + height);
 //        System.out.println("[Environment] : " + env.getProperty("my.height"));
 //        System.out.println("[ApplicationContext] : " + applicationContext.getEnvironment().getProperty("my.height"));
         System.out.println("[ConfigurationProps] : " + myProperties.getHeight());
+    }*/
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        studentService.printStudent("jack");
+        studentService.printStudent("jack");
+        studentService.printStudent("jack");
     }
+
 }
---
+++ src/main/java/com/fastcampus/springbootpractice/repository/StudentRepository.java
@@ -0,0 +1,27 @@
+package com.fastcampus.springbootpractice.repository;
+
+import com.fastcampus.springbootpractice.domain.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Repository
+public class StudentRepository {
+
+    private final Map<String, Student> storage;
+
+    // Cache 로 등록함으로써 실질적으로 이 함수는 3번 호출함에도 불구하고 1번밖에 실행안됨.
+    @Cacheable("student")
+    public Student getStudent(String name) {
+        System.out.println("[repo] 나의 통행료는 무척 비싸다!");
+        return storage.get(name);
+    }
+
+    public void enroll(String name, Integer age, Student.Grade grade) {
+        storage.put(name, Student.of(name, age, grade));
+    }
+
+}
